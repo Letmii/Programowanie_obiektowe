eabbb9e3721f7ddfd310e037302fac7fd27f3033
Bartłomiej Sereda <bartlomiej.sereda@microsoft.wsei.edu.pl>
2022-03-10 11:38:08 +0100

    testowanie klasy Student

--- lab_01/ConsoleApp1/Program.cs
+++ lab_01/ConsoleApp1/Program.cs
@@ -5,8 +5,47 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+
+
+
+        }
+        
+
+        public class Ulamek
+        {
+            private int Licznik { get; set; }
+            private int Mianownik { get; set; }
+
+            public Ulamek(int licznik, int mianownik)
+            {
+                this.Licznik = licznik;
+                this.Mianownik = mianownik;
+            }
+
+            
+            public Ulamek()
+            {
+               
+            }
+
+
+            public class Student
+            {
+                private string name;
+
+                public Student(string name)
+                {
+                    this.name = name;
+                }
+
+                public Student(Student student)
+                {
+                    this.name = student.name;
+                }
+            }
+
 
         }
     }
