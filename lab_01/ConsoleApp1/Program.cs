7dcd4194bcd1459f24978746c4656da085735fe6
Bartłomiej Sereda <bartlomiej.sereda@microsoft.wsei.edu.pl>
2022-03-11 09:46:27 +0100

    lab2

--- lab_01/ConsoleApp1/Program.cs
+++ lab_01/ConsoleApp1/Program.cs
@@ -7,11 +7,91 @@ namespace ConsoleApp1
         static void Main(string[] args)
 
         {
+            Object obj1 = new Object();
+            Console.WriteLine(obj1);
 
-
+            var a = new Fraction(5, 4);
+            var b = new Fraction(1, 2);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
 
         }
-        
+
+
+
+        public class Student: IEquatable<Student>
+        {
+            private string name;
+            private int age;
+            
+            public Student(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+            public bool Equals(Student other)
+            {
+                if (other == null) return false;
+                if (other == this) return true;
+
+                return Object.Equals(this.name, other.name) && Object.Equals(this.age, other.age);
+            }
+        }
+
+
+        public readonly struct Fraction
+        {
+            private readonly int num;
+            private readonly int den;
+
+            public Fraction(int numerator, int denominator)
+            {
+                if (denominator == 0)
+                {
+                    throw new ArgumentException("Denominator cannot be zero.", nameof(denominator));
+                }
+                num = numerator;
+                den = denominator;
+            }
+            public static Fraction operator +(Fraction a) => a;
+            public static Fraction operator -(Fraction a) => new Fraction(-a.num, a.den);
+
+            public static Fraction operator +(Fraction a, Fraction b)
+                => new Fraction(a.num * b.den + b.num * a.den, a.den * b.den);
+
+            public static Fraction operator -(Fraction a, Fraction b)
+                => a + (-b);
+
+            public static Fraction operator *(Fraction a, Fraction b)
+                => new Fraction(a.num * b.num, a.den * b.den);
+
+            public static Fraction operator /(Fraction a, Fraction b)
+            {
+                if (b.num == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                return new Fraction(a.num * b.den, a.den * b.num);
+            }
+
+            public override string ToString() => $"{num} / {den}";
+        }
+
+
+
+        public class Object
+        {
+
+
+            public override string ToString()
+            {
+                return "something";
+            }
+        }
+
+
 
         public class Ulamek
         {
@@ -24,29 +104,30 @@ namespace ConsoleApp1
                 this.Mianownik = mianownik;
             }
 
-            
+
             public Ulamek()
             {
-               
+
             }
 
 
-            public class Student
+            public class Kon
             {
                 private string name;
 
-                public Student(string name)
+                public Kon(string name)
                 {
                     this.name = name;
                 }
 
-                public Student(Student student)
+                public Kon(Kon kon)
                 {
-                    this.name = student.name;
+                    this.name = kon.name;
                 }
             }
 
 
         }
+
     }
 }
