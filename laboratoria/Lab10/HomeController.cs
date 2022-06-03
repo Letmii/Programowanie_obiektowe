bcc14a06005d2ac52be35090c86a9880fd6fc304
Bartłomiej Sereda <bartlomiej.sereda@microsoft.wsei.edu.pl>
2022-06-03 08:40:36 +0200

    Lab10

---
+++ laboratoria/Lab10/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PublicSoapAPIsServiceReference;
+using System;
+using System.Threading.Tasks;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConversionController : ControllerBase
+    {
+        // http://localhost:2988/conversion/number-to-words?number=100
+        //
+        [HttpGet("/conversion/number-to-words")]
+        public async Task<String> Get([FromQuery(Name = "number")] ulong number)
+        {
+            NumberConversionSoapTypeClient client = new NumberConversionSoapTypeClient(NumberConversionSoapTypeClient.EndpointConfiguration.NumberConversionSoap);
+
+            NumberToWordsResponse response = await client.NumberToWordsAsync(number);
+
+            return response.Body.NumberToWordsResult;
+        }
+    }
+}
